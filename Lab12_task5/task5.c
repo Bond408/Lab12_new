cc8521c7b3547adeb42364771fc4b3dda8d08878
minsiyagi <59338397+Bond408@users.noreply.github.com>
2024-12-11 18:51:22 +0300

    task5.c

--- Lab12_task5/task5.c
+++ Lab12_task5/task5.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 
-void main(void) {
-	int x = 0x12345678;//переменная инициализированная значением 0x12345678
-	int* prt; //указатель для исследуемой переменной
-	unsigned char* a1, * a2, * a3, * a4; //указатели на байты
-	unsigned char* a; //указатель на байт
-	prt = &x;//получение адреса переменной
-	a = (unsigned char*)prt;//преобразование типа для получения указателя
+void printbytes(void* ptr, int size) {
+	unsigned char* cptr = (unsigned char*)ptr;
+	for (int i = 0; i < size; i++) {
+		printf("%.2x ", cptr[i]);
+	}
+	printf("\n");
+}
 
-	a1 = a + 3;// получение 4-го байта числа х
+void main() {
+	int x = 0x12345678;
+	int* prt;
+	unsigned char *a1, *a2, *a3, *a4;
+	unsigned char *a;
+
+	prt = &x;
+	a = (unsigned char*)prt;
+
+	a1 = a + 3;
 	a2 = a + 2;
 	a3 = a + 1;
-	a4 = a; // получение первого байта числа х
+	a4 = a;
 	printf("%.2x\n%.2x\n%.2x\n%.2x\n", *a1, *a2, *a3, *a4);
+
+	int z = 12345;
+	float f = 12345;
+	double d = 12345;
+
+	printf("int:\n");
+	printbytes(&z, sizeof(z));
+
+	printf("float:\n");
+	printbytes(&f, sizeof(f));
+
+	printf("double:\n");
+	printbytes(&d, sizeof(d));
+
 }
